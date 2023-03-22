2d43e454e737e6d2cc173fa6f507e31cf0abb729
FateenAnam <fateen.rafid@vanderbilt.edu>
2023-03-22 05:21:49 -0500

    Connect Wallet moved to Login

---
+++ musicmint/components/Auth/navbar.tsx
@@ -0,0 +1,41 @@
+import React, {useContext} from 'react'
+import styles from '../../styles/marketplace.module.css'
+import Link from 'next/link'
+import AuthContext from '../../src/context/auth'
+import Image from 'next/image'
+import logo from '../../music-mint-marketplace.png'
+import {Button} from "react-bootstrap";
+
+const NavBar = (props) => {
+
+  let {user, logoutUser, authTokens} = useContext(AuthContext)
+    return (
+        <nav className={styles.nav}>
+
+          <div className = 'leftside'>
+            <li>
+              <Link href="/">
+                <Image src={logo} alt="logo" width={263.16} height={25.79}/>
+              </Link>
+              {/* <Link href="/">Logo</Link> */}
+            </li>
+          </div>
+
+          <div className='rightside'>
+            <li>
+              <Link href="/artistpage">FOR ARTISTS</Link>
+              <Link href="/marketplace">MARKETPLACE</Link>
+
+              {user ? (
+                <p onClick={logoutUser}>LOGOUT</p>
+              ) : (
+              <Link href="/auth">LOGIN</Link>
+              )}
+            </li>
+          </div>
+            
+        </nav>
+    )
+}
+
+export default NavBar
